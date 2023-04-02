e68347fcf9d524495f335fe92098047776501f1a
AserPhantom <anaaaslnwadmosiba23@gmail.com>
2023-04-02 19:22:14 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 37, 108, 165);
+const kLogo = 'assets/images/scholar.png';
+const kMessagesCollections = 'messages';
+const kMessage = 'message';
+const kCreatedAt = 'createdAt';
